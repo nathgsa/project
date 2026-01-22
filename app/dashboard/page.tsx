ce81dfce5ef3d1044076b2d2a6508d35dc4abc69
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-01-22 14:44:41 +0800

    add new file

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { lusitana } from '@/app/ui/fonts';
+ 
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+    </main>
+  );
 }
