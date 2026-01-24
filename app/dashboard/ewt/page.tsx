f7d4e1e47bc15dcbffd52b3029c62fb93ba8ac42
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-01-24 11:44:33 +0800

    modified file

--- app/dashboard/ewt/page.tsx
+++ app/dashboard/ewt/page.tsx
@@ -3,7 +3,6 @@ import EWTCalculator from "@/app/components/ewt/EWTCalculator"
 export default async function Page() {
   return (
     <div className="p-10">
-      <h1 className="text-3xl font-bold mb-6">EWT Calculator</h1>
       <EWTCalculator />
     </div>
   )
