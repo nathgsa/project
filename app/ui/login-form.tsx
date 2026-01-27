22bb3d2cc7d20df42b2e5d59da13f9b18d64737b
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-01-27 14:59:27 +0800

    modified file

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -26,7 +26,7 @@ export default function LoginForm() {
           className="w-full"
         >
           <AtSymbolIcon className="mr-2 h-5 w-5" />
-          Continue with Google
+          Sign in with Google
           <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
       </div>
