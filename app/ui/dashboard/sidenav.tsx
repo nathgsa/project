eaa6315e59adf4c58c6337d722108ecd44590202
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-01-29 13:30:07 +0800

    modified file

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -24,7 +24,7 @@ export default function SideNav() {
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-20 items-end rounded-md bg-blue-900 p-4 md:h-40"
-        href="/"
+        href="/dashboard"
       >
         <div className="w-32 text-white md:w-40">
           <AppLogo />
