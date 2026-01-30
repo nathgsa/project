9f1ad30f8af32eea1bd92d3367038bf23ec7f3f2
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-01-30 14:50:27 +0800

    modified

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,10 +20,11 @@ export default async function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-900 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AppLogo />
-          </div>
+        <div className="flex h-20 w-full items-end rounded-lg  p-3 md:h-36">
+          {/* LOGO */}
+                <div className="mb-10 flex items-center justify-center">
+                  <AppLogo showText={false} size={350} />
+                </div>
         </div>
         <Suspense>
           <LoginForm />
