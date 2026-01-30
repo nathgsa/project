91f66b3ccf29f2e3f63794ca451bc99c8d43e24f
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-01-30 14:25:19 +0800

    modified

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
         <AppLogo showText={false} size={400} />
       </div>
 
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row item-center">
         <div className="flex flex-col justify-center item-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className="text-l text-gray-800 md:text-3xl md:leading-normal">
             <strong>Welcome to Optima Typographics Calculators.</strong>
