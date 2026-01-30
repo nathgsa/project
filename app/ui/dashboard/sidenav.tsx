1a94f3774b4c286ad91e18d6cd56eaa2ec22c27b
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-01-30 14:52:46 +0800

    modified

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -13,9 +13,9 @@ export default function SideNav() {
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-900 p-4 md:h-40"
         href="/dashboard"
       >
-        <div className="w-32 text-white md:w-40">
-          <AppLogo />
-        </div>
+        <div className="w-32 md:w-40 flex">
+                          <AppLogo showText={false} size={200} />
+                        </div>
       </Link>
 
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
