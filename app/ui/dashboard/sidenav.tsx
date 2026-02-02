a5a8fafb18c7664f391040be9e210831c3ed950a
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-02-02 14:17:08 +0800

    modified

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -9,6 +9,7 @@ import { signOutAction } from "@/app/actions/auth-action";
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-sky-200">
+      <br />
       <Link
         className="flex h-20 w-full md:h-40 items-center justify-center"
         href="/dashboard"
