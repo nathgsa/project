8da8b286969114024112001d8a03aa9cfc506632
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-02-04 08:56:40 +0800

    modified

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,10 +7,10 @@ import LoginForm from '@/app/ui/login-form';
 export default function LoginPage() {
   return (
     <main className="flex min-h-screen items-center justify-center p-4 bg-gray-50">
-      <div className="flex w-full max-w-[400px] flex-col items-center justify-center space-y-4 -mt-10">
+      <div className="flex w-full max-w-[800px] flex-col items-center justify-center space-y-4 -mt-10">
         
         {/* Logo */}
-        <div className="flex items-center justify-center -mb-2">
+        <div className="flex items-center justify-center -mb-2 space-y-4">
           <AppLogo showText={false} size={280} />
         </div>
 
