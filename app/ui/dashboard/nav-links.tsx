ca7b7e71cfb5482c5c7594c47507063388b78d77
Nathalie Grace Acojedo <nathaliegraceacojedo@gmail.com>
2026-02-06 13:55:59 +0800

    m

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -37,7 +37,7 @@ export default function NavLinks({ onLinkClick }: NavLinksProps) {
             href={link.href}
             onClick={onLinkClick} // <--- closes sidebar on mobile
             className={clsx(
-              'flex h-[48px] grow gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex h-[48px] grow gap-2 items-center rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
